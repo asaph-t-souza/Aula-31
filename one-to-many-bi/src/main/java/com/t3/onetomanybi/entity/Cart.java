14873ff1e85e72e70de77c2815ec52fcf686edde
asaph-t-Souza <asaph.net@gmail.com>
2023-04-27 19:51:36 -0300

    init

---
+++ one-to-many-bi/src/main/java/com/t3/onetomanybi/entity/Cart.java
@@ -0,0 +1,12 @@
+package com.t3.onetomanybi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cart_bi")
+@Getter @Setter @NoArgsConstructor
+public class Cart {
+    
+}
+
---
+++ one-to-many-bi/src/test/java/com/t3/onetomanybi/OneToManyBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t3.onetomanybi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
